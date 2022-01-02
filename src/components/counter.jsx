b23046cea0e0bce9103d8dc583bb319b17a2ef59
Joyjk <17-35526-3@student.aiub.edu>
2022-01-02 16:25:58 +0600

    update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -64,7 +64,7 @@ class Counter extends React.Component {
   // }
   getBadgeChanges() {
     let classes = "badge m-2 badge-";
-    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "success";
     return classes;
   }
 
